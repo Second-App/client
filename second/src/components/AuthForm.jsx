d59ae92b4b2ba71351440bbbfa4ce77ac7a523bd
rbernanda <acappemenang@gmail.com>
2021-04-16 18:29:35 +0700

    Auth Form

--- second/src/components/AuthForm.jsx
+++ second/src/components/AuthForm.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+}
 
 export default function AuthForm() {
+  const [authForm, setAuthForm] = useState(false)
+  const [isLogin, setIsLogin] = useState(false)
+
+  const openModal = () => {
+    setAuthForm(true)
+  }
+
+  const closeModal = () => {
+    setAuthForm(false)
+  }
+
+  const handleLogout = () => {
+    setIsLogin(false)
+  }
+
+  const handleRegister = () => {
+
+  }
+
+  const handleLogin = () => {
+
+  }
+
   return (
     <div>
-      auth form
+      {isLogin ? (
+        <button className="button is-primary" onClick={handleLogout}>
+          Logout
+        </button>
+      ) : (
+        <>
+          <button className="button is-primary" onClick={openModal}>
+            Login
+          </button>
+          <button className="button is-primary" onClick={openModal}>
+            Sign Up
+          </button>
+        </>
+      )}
+      <Modal
+        isOpen={authForm}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input className="input" type="email" placeholder="Email input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="password input" />
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="label">ImageURL</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="URL input" />
+          </div>
+        </div>
+        
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="number" placeholder="number input" />
+          </div>
+        </div>
+
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Submit</button>
+          </div>
+          <div className="control">
+            <button className="button is-light" onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
--- second/src/components/Navbar.jsx
+++ second/src/components/Navbar.jsx
@@ -1,68 +1,84 @@
 import React from 'react'
-import {ProductForm} from './index'
+import { NavLink } from 'react-router-dom'
+import { ProductForm, AuthForm } from './index'
 
 export default function Navbar() {
   return (
-    <nav className="navbar is-transparent box-content" role="navigation" aria-label="main navigation">
+    <nav
+      className="navbar box-content"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <div className="navbar-brand">
-        <a className="navbar-item" href="">
-          <img src="https://secondh8.s3-ap-southeast-1.amazonaws.com/logo/second.png"/>
-        </a>
+        <NavLink className="navbar-item" to="/">
+          <img src="https://secondh8.s3-ap-southeast-1.amazonaws.com/logo/second.png" />
+        </NavLink>
 
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">
-              Home
-            </a>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <NavLink exact to="/" className="navbar-item">
+            Home
+          </NavLink>
 
-            <a className="navbar-item">
-              Documentation
-            </a>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">Categories</a>
 
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">
-                More
-               </a>
+            <div className="navbar-dropdown">
+              <NavLink to="/category/2" className="navbar-item">
+                Fashion
+              </NavLink>
+              <NavLink to="/category/3" className="navbar-item">
+                Technology
+              </NavLink>
+              <NavLink to="/category/4" className="navbar-item">
+                Contact
+              </NavLink>
+            </div>
+          </div>
 
-              <div className="navbar-dropdown">
-                <a className="navbar-item">
-                  About
-                </a>
-                <a className="navbar-item">
-                  Jobs
-                </a>
-                <a className="navbar-item">
-                  Contact
-                </a>
-                <hr className="navbar-divider"/>
-                  <a className="navbar-item">
-                    Report an issue
-                  </a>
-              </div>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">Types</a>
+
+            <div className="navbar-dropdown">
+              <NavLink to="/type/2" className="navbar-item">
+                Lelang
+              </NavLink>
+              <NavLink to="/type/3" className="navbar-item">
+                Jual-Beli
+              </NavLink>
+              <NavLink to="/type/4" className="navbar-item">
+                Gratis
+              </NavLink>
+            </div>
           </div>
         </div>
 
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons">
-                  <a className="button is-primary">
-                    <strong>Sign up</strong>
-                  </a>
-                  <a className="button is-ghost" style={{color:'white', backgroundColor: '#ed7014'}}>
-                    Log in
-                  </a>
-                  <ProductForm/>
-                </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <div className="mr-2">
+                <AuthForm />
+              </div>
+              <div className="mr-2">
+                <ProductForm />
               </div>
             </div>
           </div>
+        </div>
+      </div>
     </nav>
   )
 }
