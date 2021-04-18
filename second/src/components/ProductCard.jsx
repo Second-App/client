ed429ca175f8700fecf9e95137f971fbb9095b5b
J-R4 <JoshuaFanera@gmail.com>
2021-04-18 15:37:32 +0700

    completing all the images-bank for all the product, type, and categories, perfecting in the type/pages

--- second/src/components/ProductCard.jsx
+++ second/src/components/ProductCard.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export default function ProductCard() {
+export default function ProductCard(props) {
+  const { data } = props
+  const e = data
   return (
-    <div>
-      ini product card
+    <div className="column is-one-fifth mt-2" key={e.id}>
+      <figure className="image">
+        <img
+          className="image"
+          src={e.imageUrl}
+          alt={e.name}
+          style={{
+            borderRadius: '20px',
+            boxShadow: '0px 0px 7px #FF8D2D',
+            height: '250px',
+            width: '250px',
+            objectFit: 'cover',
+            cursor: 'pointer',
+          }}
+        />
+      </figure>
+      <p
+        className="subtitle"
+        style={{ textAlign: 'left', marginTop: '15px' }}
+      >
+        {e.name}
+      </p>
     </div>
   )
 }
