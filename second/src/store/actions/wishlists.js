617988bb18af8dc5ae29a8d5b1e79339646fc96f
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2021-04-20 21:17:35 +0700

    comment line to remove infinite loop on ProductCard && handle error toast on signup, login, and bid failure

--- second/src/store/actions/wishlists.js
+++ second/src/store/actions/wishlists.js
@@ -1,39 +1,51 @@
-import axios from '../../axios'
-import { SET_WISHLIST, ADD_WISHLIST, REMOVE_WISHLIST, SET_LOADING, SET_ERROR} from '../types'
+import axios from '../../axios';
+import {
+  SET_WISHLIST,
+  ADD_WISHLIST,
+  REMOVE_WISHLIST,
+  SET_LOADING,
+  SET_ERROR,
+} from '../types';
+import { toast } from 'react-toastify';
 
 export function fetchWishlist() {
   return async (dispatch) => {
     try {
       const { data } = await axios.get('/wishlists/', {
-        headers: { access_token: localStorage.access_token}
-      })
-      await dispatch(SET_WISHLIST(data))
-      await dispatch(SET_LOADING(false))
+        headers: { access_token: localStorage.access_token },
+      });
+      await dispatch(SET_WISHLIST(data));
+      await dispatch(SET_LOADING(false));
     } catch (error) {
-      await dispatch(SET_ERROR(error))
+      await dispatch(SET_ERROR(error));
     }
-  }
+  };
 }
 
 export function addToWishlist(payload) {
   return async (dispatch, getState) => {
     try {
-      
-      const {data} = await axios({
+      const { data } = await axios({
         url: '/wishlists/',
         headers: { access_token: localStorage.access_token },
         data: {
-          ProductId: payload.id
+          ProductId: payload.id,
         },
-        method: 'post'
-      })
-      await dispatch(ADD_WISHLIST(data))
-      
+        method: 'post',
+      });
+      await dispatch(ADD_WISHLIST(data));
     } catch (error) {
-      
-      dispatch(SET_ERROR(error)) 
+      toast.error('Product already in your wishlist', {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
-  }
+  };
 }
 
 export function deleteWishlist(id) {
@@ -41,14 +53,13 @@ export function deleteWishlist(id) {
     try {
       const response = await axios.delete(`/wishlists/${id}`, {
         headers: {
-          access_token:localStorage.access_token
-        }
-      })
-      
-      if (response.status === 200) dispatch(fetchWishlist())
+          access_token: localStorage.access_token,
+        },
+      });
+
+      if (response.status === 200) dispatch(fetchWishlist());
     } catch (error) {
-      
-      dispatch(error)
+      dispatch(error);
     }
-  }
+  };
 }
