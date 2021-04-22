024ee7e6015a36a858fc7e1d5b40742669775b95
J-R4 <JoshuaFanera@gmail.com>
2021-04-22 11:19:53 +0700

    setting up for presentation

--- second/src/axios/index.js
+++ second/src/axios/index.js
@@ -3,6 +3,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://second-h8.herokuapp.com/',
-    //baseURL: 'http://localhost:3001' // for local only
+    // baseURL: 'https://second-h8.herokuapp.com/',
+    baseURL: 'http://localhost:3001', // for local only
 });
